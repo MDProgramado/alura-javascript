fc1cf5ecc5cbf4849d7186b480e9952e8e059280
Maicon Douglas <mdprogamacao@outlook.com>
2025-03-14 23:01:08 -0300

    coloando sons nos botões

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,53 @@
-querySelector('.tecla_pom');
-document.querySelector('.tecla_pom');
 
-function tecla_pom(tecla_pom) {
-  alert('Fui clicado!');
+document.querySelector('.tecla_splash')
+document.querySelector('.tecla_toim');
+document.querySelector('.tecla_psh');
+document.querySelector('.tecla.tic');
+document.querySelector('.tecla_tom');
+
+
+function toca_som_pom() {
+  document.querySelector('#som_tecla_pom').play();  
 }
+document.querySelector('.tecla_pom').onclick = toca_som_pom;
+
+function toca_som_clap() {
+  document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = toca_som_clap;
+
+function toca_som_tim() {
+  document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = toca_som_tim;
+
+function toca_som_puff() {
+  document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = toca_som_puff;
+
+function toca_som_spash() {
+  document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = toca_som_spash;
+
+function toca_som_toim() {
+  document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = toca_som_toim;
+
+function toca_som_psh () {
+  document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick = toca_som_psh;
+
+function toca_som_tic() {
+  document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = toca_som_tic;
+
+function toca_som_tom() {
+  document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = toca_som_tom;
