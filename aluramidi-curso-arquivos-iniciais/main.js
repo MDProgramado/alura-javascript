7cd2388cc4f330caff76ecc5ef3d6e0f3d1d4d98
Maicon Douglas <mdprogamacao@outlook.com>
2025-03-17 20:34:01 -0300

    terminando primeiro curso da js, usando funções, estruturas de repetições, condicinais if e else, criando codigo automatizado que percorra todo o codigo

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,53 +1,38 @@
+function tocaSom (seletorAudio) {
+  const elemento =  document.querySelector(seletorAudio); 
 
-document.querySelector('.tecla_splash')
-document.querySelector('.tecla_toim');
-document.querySelector('.tecla_psh');
-document.querySelector('.tecla.tic');
-document.querySelector('.tecla_tom');
+  if(elemento && elemento.localName === 'audio') {
+      elemento.play();
+    }
+    
+    else {
+      alert('Elemento não encontrado ou seletor inválido');
 
-
-function toca_som_pom() {
-  document.querySelector('#som_tecla_pom').play();  
-}
-document.querySelector('.tecla_pom').onclick = toca_som_pom;
-
-function toca_som_clap() {
-  document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = toca_som_clap;
-
-function toca_som_tim() {
-  document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = toca_som_tim;
-
-function toca_som_puff() {
-  document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = toca_som_puff;
-
-function toca_som_spash() {
-  document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = toca_som_spash;
-
-function toca_som_toim() {
-  document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = toca_som_toim;
-
-function toca_som_psh () {
-  document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = toca_som_psh;
-
-function toca_som_tic() {
-  document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = toca_som_tic;
-
-function toca_som_tom() {
-  document.querySelector('#som_tecla_tom').play();
+  
 }
 
-document.querySelector('.tecla_tom').onclick = toca_som_tom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function() {
+    tocaSom(idAudio);
+  }
+
+  tecla.onkeydown = function(evento) {
+
+    if(evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+
+  }
+
+  tecla.onkeyup = function(evento) {
+    tecla.classList.remove('ativa');
+  }
+}
