5ceff65d2f0fbe47d1c5c0de92df740ec7e96868
Maicon Douglas <mdprogamacao@outlook.com>
2025-04-27 00:36:04 -0300

    usando metodo ForEach

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,38 +1,33 @@
-function tocaSom (seletorAudio) {
-  const elemento =  document.querySelector(seletorAudio); 
-
-  if(elemento && elemento.localName === 'audio') {
-      elemento.play();
-    }
-    
-    else {
-      alert('Elemento não encontrado ou seletor inválido');
-
-  
+// Alura Midi - Curso de JavaScript: Crie um MIDI Player com JavaScript
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    alert('Elemento não encontrado ou seletor inválido');
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+listaDeTeclas.forEach(tecla => {
+  const instrumento = tecla.classList[1];           
+  const idAudio    = `#som_${instrumento}`;        
 
-  const tecla = listaDeTeclas[contador];
-  instrumento = tecla.classList[1];
 
-  const idAudio = `#som_${instrumento}`;
-
-  tecla.onclick = function() {
+  tecla.addEventListener('click', () => {
     tocaSom(idAudio);
-  }
+  });
 
-  tecla.onkeydown = function(evento) {
 
-    if(evento.code === 'Space' || evento.code === 'Enter') {
+  tecla.addEventListener('keydown', evento => {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
       tecla.classList.add('ativa');
+      tocaSom(idAudio);
     }
+  });
 
-  }
-
-  tecla.onkeyup = function(evento) {
+  tecla.addEventListener('keyup', () => {
     tecla.classList.remove('ativa');
-  }
-}
+  });
+});
